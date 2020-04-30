770adddf6839fb7c2283f3aa6ef8e7132a7172e6
chenws <single_tigger@163.com>
2020-04-30 14:44:42 +0800

    init

---
+++ src/test/java/com/chenws/docker/springbootdocker/SpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chenws.docker.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
