f9ca485e01acbda52589e92c1b62fbb60d22423e
chenws <single_tigger@163.com>
2020-05-04 10:51:30 +0800

    init

--- src/main/java/com/chenws/docker/controller/TestController.java
+++ src/main/java/com/chenws/docker/controller/TestController.java
@@ -1,12 +1,12 @@
 package com.chenws.docker.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by chenws on 2020/4/30.
  */
-@RestController
+@Controller
 public class TestController {
 
     @GetMapping("/")
