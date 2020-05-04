fa6b533a4e16cc70dca8a5d68f608324b59bec70
chenws <single_tigger@163.com>
2020-05-04 10:35:56 +0800

    init

--- src/main/java/com/chenws/docker/controller/TestController.java
+++ src/main/java/com/chenws/docker/controller/TestController.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping("/test")
+    @GetMapping("/")
     public String test(){
-        return "success";
+        return "index";
     }
 
 }
